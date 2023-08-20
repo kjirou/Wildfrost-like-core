808d0b1f2465d670377076110e84ea3d48c04a14
kjirou <kjirou.web@gmail.com>
2023-08-20 14:06:36 +0900

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,43 @@
+// 色々な行動(FieldObjectAction)のタイミング
+//
+// フィールドオブジェクトがtimer0で発動するもの
+// 攻撃に対するリアクションで発動するもの、オリジナルのオカエシ
+// 攻撃に対して付随して発動するもの、オリジナルのメガハンマー？だっけ
+
+type FieldObjectAction = {
+  repeats: number;
+  targetting: "back" | "front" | "low" | "random";
+};
+
+type StateChange = {};
+
+type PassiveSkill = {
+  kind: "addionalStateChange" | "reactionToAttack";
+  power: number;
+};
+
 /**
  * フィールドのマスを占有するオブジェクト
  *
  * オリジナルだとキャラクターまたはクランカーに相当する。
  *
- * 1 オブジェクトが複数マスを占有することがある。オリジナルだと縦2マスのみ。
+ * TODO: 1 オブジェクトが複数マスを占有することがある。オリジナルだと縦2マスのみ。
  */
 type FieldObject = {
+  action: FieldObjectAction;
+  actionWait: number;
   attackPoints: number;
-  buffs: Array<any>;
+  elapsedActionWait: number;
   id: string;
   lifePoints: number;
   maxAttackPoints: number;
   maxLifePoints: number;
   maxShieldScrapingPoints: number;
-  passiveSkills: Array<any>;
+  passiveSkills: Array<PassiveSkill>;
   shieldScrapingPoints: number;
   shieldPoints: number;
+  stateChanges: Array<StateChange>;
+  stunPoints: number;
 };
 
 /**
